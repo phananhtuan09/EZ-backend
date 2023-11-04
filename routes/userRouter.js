87e5f53ec8b4b2ae9f77362ba696b1d4b2c1c30d
phananhtuan09 <tuanp6905@gmail.com>
2023-11-04 16:22:21 +0700

    fix logic response not found

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,11 +3,13 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const { catchErrors } = require("../middleware/errorHandlers");
 const { uploadAvatar } = require("../config/multerUpload");
+const verifyAccessToken = require("../middleware/verifyAccessToken");
 
 const router = express.Router();
 
 router.post(
   "/updateAvatar",
+  verifyAccessToken,
   uploadAvatar.single("file"),
   catchErrors(userController.handleUpdateAvatar)
 );
